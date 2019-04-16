643e0443b4a5523be97244c695b8f8ae51d069b2
DaemonSpelledWrong <damongself@gmail.com>
2019-04-16 19:56:47 -0400

    Uploaded high scores exercism

---
+++ high_scores.rb
@@ -0,0 +1,21 @@
+class HighScores
+  def initialize scores
+    @scores = scores
+  end
+
+  def scores
+    @scores
+  end
+
+  def latest
+    @scores.pop
+  end
+
+  def personal_best
+    @scores.max
+  end
+
+  def personal_top_three
+    @scores.max(3)
+  end
+end
